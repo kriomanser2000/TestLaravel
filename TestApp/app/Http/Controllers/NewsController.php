31bb4e18b84b3f70b6b9dd3d881fab66aa00450a
Kriomanser2000 <kuzmenkasana05@gmail.com>
2024-09-08 15:42:33 +0300

    edited NewsController

--- TestApp/app/Http/Controllers/NewsController.php
+++ TestApp/app/Http/Controllers/NewsController.php
@@ -25,4 +25,25 @@ class NewsController extends Controller
         ]);
         return redirect('/tables');
     }
+    public function update(Request $request, $id)
+    {
+        $news = News::findOrFall($id);
+        $data = $request->validate([
+            'summary'=>'required|max:50',
+            'short_description'=>'required|max:150',
+            'full_text'=>'required|max:5000',
+            'image'=>'nullable|image|max:2048',
+        ]);
+        if($request->hasFile('image'))
+        {
+            $data['image'] = file_get_contents($request->file('image')->getRealPath());
+        }
+        $news->update($data);
+        return redirect('/');
+    }
+    public function show($id)
+    {
+        $news = News::findOrFall($id);
+        return view('newView', compact('news'));
+    }
 }
