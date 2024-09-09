12f35fe404be8c3b933200c1933b0ea3cecb7c86
Kriomanser2000 <kuzmenkasana05@gmail.com>
2024-09-09 17:19:14 +0300

    added new Table

---
+++ TestApp/database/migrations/2024_09_09_141425_create_comments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB; 
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) 
+        {
+            $table->id();
+            $table->unsignedBigInteger('id_user');
+            $table->text('comment');
+            $table->timestamp('date')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
